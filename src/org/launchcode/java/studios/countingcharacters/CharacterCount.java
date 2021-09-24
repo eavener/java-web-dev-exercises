030da3c1fe4473f5589f631c1551e0b9abc3e5a5
avener <eavener@gmail.com>
2021-09-23 20:42:07 -0400

    Closed scanner

--- src/org/launchcode/java/studios/countingcharacters/CharacterCount.java
+++ src/org/launchcode/java/studios/countingcharacters/CharacterCount.java
@@ -9,6 +9,7 @@ public class CharacterCount {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter a phrase: ");
         String phrase = input.nextLine();
+        input.close();
         //String phrase = "If the product of two terms is zero then common sense says at least one of the two terms has to be zero to start with. So if you move all the terms over to one side, you can put the quadratics into a form that can be factored allowing that side of the equation to equal zero. Once you’ve done that, it’s pretty straightforward from there.";
         char[] charactersInString = phrase.toCharArray();
         for (char c : charactersInString){
